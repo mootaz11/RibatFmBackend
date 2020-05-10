a28bfc02634fc0a66752825aae98f9b610f6423a
amara <amaramootaz11@gmail.com>
2020-05-10 22:52:00 +0200

    last commit

--- routes/episode.js
+++ routes/episode.js
@@ -3,11 +3,14 @@ const episodeController = require("../controllers/episodeController");
 const multer_son=require("../config/multer_son");
 
 
+
+router.get("/getepisodes",episodeController.getAllEpisodes);
 router.post("/createEpisode/:idpodcast",multer_son.single("sequence"),episodeController.createEpisode);
 router.post("/update/:idepisode",multer_son.single("sequence"),episodeController.updateEpisode);
 router.get("/getEpisode/:idpodcast",episodeController.getEpisodesbyPodcast);
 router.delete("/deleteEpisode/:idepisode",episodeController.delete_episode);
 router.get("/:idepisode",episodeController.getEpisode);
+
 
 
 module.exports=router;
