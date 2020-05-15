0711703f48f6bc919c2b84fff747e4685cd38fa3
amara <amaramootaz11@gmail.com>
2020-05-15 21:58:58 +0200

    last one please

--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,5 @@ router.post("/login", AdminController.login);
 router.post("/update/:id", AdminController.updateInfo);
 router.delete("/deleteAdmin/:id", AdminController.delete_Sous_Admin)
 router.get("/Sous_admins", AdminController.All_sub_admins)
-
+router.get("/allAdmins",AdminController.all_admins)
 module.exports = router;
--- routes/comment.js
+++ routes/comment.js
@@ -3,4 +3,5 @@ const commentController = require("../controllers/commentController");
 router.get("/allComments",commentController.showComments);
 router.get("/comments/:idactualite",commentController.showCommentsByActualite)
 router.post("/createComment/:idactualite",commentController.createComment);
+router.delete("/deleteComment/:idcomment",commentController.deleteComment)
 module.exports=router;
