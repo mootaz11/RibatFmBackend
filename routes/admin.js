7a9a27d1d97f600e77b7c603d1e639a9f5d0eb90
amara <amaramootaz11@gmail.com>
2020-05-17 13:17:53 +0200

    hello

--- routes/admin.js
+++ routes/admin.js
@@ -8,4 +8,5 @@ router.post("/update/:id",authentication,AdminController.updateInfo);
 router.delete("/deleteAdmin/:id",authentication,AdminController.delete_Sous_Admin)
 router.get("/Sous_admins",authentication,AdminController.All_sub_admins)
 router.get("/allAdmins",authentication,AdminController.all_admins)
+
 module.exports = router;    
